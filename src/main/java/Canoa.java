dd970e9567337de7f5a30a56fe00b9abbc0283d4
Diego <diegocarrasco1013@gmail.com>
2023-04-17 14:57:23 +0200

    Optimizado get_Shot de canoa

--- src/main/java/Canoa.java
+++ src/main/java/Canoa.java
@@ -2,4 +2,12 @@ public class Canoa extends Ship {
     public Canoa(Point punto_Partida, CardinalPoints orientacion) {
         super(1, punto_Partida, orientacion);
     }
+
+    @Override
+    public void get_Shot(Point point) {
+        if (punto_Partida.x == point.x && punto_Partida.y == point.y){
+            System.out.println("¡Tocado!");
+            vida--;
+        }
+    }
 }
